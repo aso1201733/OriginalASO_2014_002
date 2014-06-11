ccfe1c9c2aa360eba36113d53cc19f37cef8d35e
student <student@PC111S41.fgakusei.local>
2014-06-11 11:19:39 +0900

    DB3途中

--- src/com/example/originalaso_2014_002/MainActivity.java
+++ src/com/example/originalaso_2014_002/MainActivity.java
@@ -1,18 +1,59 @@
 package com.example.originalaso_2014_002;
 
+//import jp.ac.st.asojuku.originalaso_2014_001.R;
+import android.util.Log;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
 
-public class MainActivity extends Activity  implements View.OnClickListener{
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+	public SQLiteDatabase sdb = null;
+	public MySQLiteOpenHelper helper = null;
 
 	public void onClick(View v) {
+		Intent intent = null;
+
+		switch (v.getId()) {
+		case R.id.checkButton:
+			Log.d("いべんと発生！", "でばっぐ");
+			intent = new Intent(MainActivity.this, HitokotoActivity.class);
+
+			intent.putExtra("hitokoto", "おくるあたい");
+			startActivity(intent);
+			break;
+
+		case R.id.maintenanceButton:
+			break;
+		}
+
 	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO 閾ｪ蜍慕函謌舌＆繧後◆繝｡繧ｽ繝�ラ繝ｻ繧ｹ繧ｿ繝�
+		super.onResume();
+		Button maintenanceButton = (Button) findViewById(R.id.maintenanceButton);
+		maintenanceButton.setOnClickListener(this);
+
+		Button checkButton = (Button) findViewById(R.id.checkButton);
+		checkButton.setOnClickListener(this);
+
+		Button ragistrationButton = (Button) findViewById(R.id.ragistrationButton);
+		ragistrationButton.setOnClickListener(this);
 	}
 
 	@Override
